0f81b00f172957bfdfcac34a9f9da353b34349aa
Mirec <mirectec@gmail.com>
2024-06-13 08:49:18 +0200

    new

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ function generateCertificate(domains) {
     return new Promise((resolve, reject) => {
         const domainArgs = domains.map(d => `-d ${d}`).join(' ');
         const certHome = '/var/www/certs';
-        exec(`~/.acme.sh/acme.sh --issue ${domainArgs} --webroot ${sharedWebroot} --cert-home ${certHome}`, (error, stdout, stderr) => {
+        exec(`~/.acme.sh/acme.sh --issue ${domainArgs} --webroot ${sharedWebroot} --cert-home ${certHome} --keylength 2048`, (error, stdout, stderr) => {
             if (error) {
                 if (stderr.includes('Certificate not yet due for renewal')) {
                     resolve(`No need to renew certificate for ${domains.join(', ')}: ${stderr}`);
